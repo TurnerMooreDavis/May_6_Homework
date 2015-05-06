4c3f88c379133e47657872477fee896e8d4f46ee
TurnerMooreDavis <turnermdavis@gmail.com>
2015-05-06 14:41:54 -0400

    add letter to number converter

--- blackjack_advisor.rb
+++ blackjack_advisor.rb
@@ -1,15 +1,27 @@
 puts "whats your first card?"
-first_card = gets.chomp.to_i
+first_card = gets.chomp.upcase
 puts "and your second?"
-second_card = gets.chomp.to_i
+second_card = gets.chomp.upcase
 puts "whats the dealers card?"
-dealers_card= gets.chomp.to_i
+dealers_card= gets.chomp.upcase
+
+def convert_input (card)
+  if card == "K" || card == "Q" || card == "J"
+    return 10
+  elsif card == "A"
+    return "A"
+  else
+    return card.to_i
+  end
+end
+
+
 
 cards = {
   "users_hand" => {
-    "first_card" => first_card,
-    "second_card" => second_card},
-  "dealers_card" => dealers_card
+    "first_card" => convert_input(first_card),
+    "second_card" => convert_input(second_card)},
+  "dealers_card" => convert_input(dealers_card)
 }
 
 p cards
