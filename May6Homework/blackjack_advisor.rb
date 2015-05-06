c0b9c7c7b0c01ba7098996562007aa853f91fdcd
TurnerMooreDavis <turnermdavis@gmail.com>
2015-05-06 16:53:16 -0400

    add test hash and code to navigate it

---
+++ May6Homework/blackjack_advisor.rb
@@ -0,0 +1,71 @@
+puts "whats your first card?"
+first_card = gets.chomp.upcase
+puts "and your second?"
+second_card = gets.chomp.upcase
+puts "whats the dealers card?"
+dealers_card= gets.chomp.upcase
+
+def convert_input (card)
+  if card == "K" || card == "Q" || card == "J"
+    return 10
+  elsif card == "A"
+    return "A"
+  else
+    return card.to_i
+  end
+end
+
+
+cards = {
+  "users_hand" => {
+    "first_card" => convert_input(first_card),
+    "second_card" => convert_input(second_card)},
+  "dealers_card" => convert_input(dealers_card)
+}
+
+p cards
+
+def find_hand_type (first, second)
+  if first == second
+    return "pair"
+  elsif first == "A" || second == "A"
+    return "soft"
+  else
+    return "hard"
+  end
+end
+
+hard = {
+  2..3 => {5..8 => "Hit", 9..11 => "Double or Hit", 12 => "Hit", 13..21 => "Stand"},
+  4 => {5..8 => "Hit", 9..11 => "Double or Hit", 12..21 => "Stand"},
+  5..6 => {5..7 => "Hit", 8..11 => "Double or Hit", 12..21 => "Stand"},
+  7..9 => {5..9 => "Hit", 10..11 => "Double or Hit", 12..16 => "Hit", 17..21 => "Stand"},
+  10 => {5..10 => "Hit", 11 => "Double or Hit", 12..16 => "Hit", 17..21 => "Stand"},
+  "A" => {5..10 => "Hit", 11 => "Double or Hit", 12..16 => "Hit", 17..21 => "Stand"}
+}
+puts cards["dealers_card"]
+hand = hard.select { |dealer| dealer === cards["dealers_card"] }.values.first
+puts hand
+total_hand = cards["users_hand"]["first_card"] + cards["users_hand"]["second_card"]
+answer = hand.select { |hand| hand === total_hand }.values.first
+puts answer
+
+# soft = {
+#   2 => {13..16 => "Hit", 17 => "Double or Hit", 18..21 => "Stand"},
+#   3 => {13..16 => "Hit", 17 => "Double or Hit", 18 => "Double or Stand" 19..21 => "Stand"},
+#   4..5 => {13..17 => "Double or Hit", 18 => "Double or Stand" 19..21 => "Stand"},
+#   6 => {13..17 => "Double or Hit", 18..19 => "Double or Stand" 20..21 => "Stand"},
+#   7..8 => {13..17 => "Hit", 18..21 => "Stand"},
+#   9..10 => {13..19 => "Hit", 20..21 => "Stand"},
+#   "A" => {13..17 => "Hit", 18..21 => "Stand"},
+# }
+#
+# pair = {
+#   2..3 => {2..3 => "Split", 4 => "Hit", 18..21 => "Stand"},
+#   4..6 => {13..16 => "Hit", 17 => "Double or Hit", 18 => "Double or Stand" 19..21 => "Stand"},
+#   7 => {13..17 => "Double or Hit", 18 => "Double or Stand" 19..21 => "Stand"},
+#   8 => {13..17 => "Double or Hit", 18..19 => "Double or Stand" 20..21 => "Stand"},
+#   9 => {13..17 => "Hit", 18..21 => "Stand"},
+#   10 => {13..19 => "Hit", 20..21 => "Stand"},
+#   "A" => {13..17 => "Hit", 18..21 => "Stand"},
+# }
