b00435bffdc32a09990770223e85fa18501841ff
TurnerMooreDavis <turnermdavis@gmail.com>
2015-05-07 02:06:42 -0400

    fixed bugs, dealt with Aces, fixed typos

--- May6Homework/blackjack_advisor.rb
+++ May6Homework/blackjack_advisor.rb
@@ -25,30 +25,6 @@ cards = {
 
 p cards
 
-def find_hand_type (first, second)
-  if first == second
-    return "pair"
-  elsif first == "A" || second == "A"
-    return "soft"
-  else
-    return "hard"
-  end
-end
-
-def make_suggestion (hand_type,dealer, first, second)
-puts cards["dealers_card"]
-hand = hand_type.select { |dealer| dealer === cards["dealers_card"] }.values.first
-puts hand
-if type == "soft" && first.include?("A")
-  first = 11
-if type == "soft" && second.include?("A")
-  second = 11
-end
-total_hand = cards["users_hand"]["first_card"] + cards["users_hand"]["second_card"]
-answer = hand.select { |hand| hand === total_hand }.values.first
-puts answer
-end
-
 hard = {
   2..3 => {5..8 => "Hit", 9..11 => "Double or Hit", 12 => "Hit", 13..21 => "Stand"},
   4 => {5..8 => "Hit", 9..11 => "Double or Hit", 12..21 => "Stand"},
@@ -61,22 +37,56 @@ hard = {
 
 soft = {
   2 => {13..16 => "Hit", 17 => "Double or Hit", 18..21 => "Stand"},
-  3 => {13..16 => "Hit", 17 => "Double or Hit", 18 => "Double or Stand" 19..21 => "Stand"},
-  4..5 => {13..17 => "Double or Hit", 18 => "Double or Stand" 19..21 => "Stand"},
-  6 => {13..17 => "Double or Hit", 18..19 => "Double or Stand" 20..21 => "Stand"},
+  3 => {13..16 => "Hit", 17 => "Double or Hit", 18 => "Double or Stand", 19..21 => "Stand"},
+  4..5 => {13..17 => "Double or Hit", 18 => "Double or Stand", 19..21 => "Stand"},
+  6 => {13..17 => "Double or Hit", 18..19 => "Double or Stand", 20..21 => "Stand"},
   7..8 => {13..17 => "Hit", 18..21 => "Stand"},
   9..10 => {13..19 => "Hit", 20..21 => "Stand"},
-  "A" => {13..17 => "Hit", 18..21 => "Stand"},
+  "A" => {13..17 => "Hit", 18..21 => "Stand"}
 }
 
 pair = {
-  2..3 => {2..3 => "Split", 4 => "Hit", 5 => "Double or Hit", 6..9 => "Split",10 => "Stand","A" => "Split"},
-  4..6 => {2..4 => "Split", 5 => "Double or Hit", 6..9 => "Split",10 => "Stand","A" => "Split"},
-  7 => {2..3 => "Split", 4 => "Hit", 5 => "Double or Hit", 6..9 => "Split",10 => "Stand","A" => "Split"},
-  8 => {2 => "Hit", 3 => "Split", 4 => "Hit", 5 => "Double or Hit", 6 => "Hit", 7..9 => "Split",10 => "Stand","A" => "Split"},
-  9 => {2..4 =>"Hit", 5 => "Double or Hit", 6..7 => "Hit",8..9 => "Split",10 => "Stand","A" => "Split"},
-  10 => {2..6 =>"Hit", 7 => "Split",8 => "Pair",9..10 => "Stand","A" => "Split"},
-  "A" => {2..7 =>"Hit",8 => "Pair",9..10 => "Stand","A" => "Split"},
+  2..3 => {4..6 => "Split", 8 => "Hit", 10 => "Double or Hit", 12..18 => "Split",20 => "Stand",22 => "Split"},
+  3..6 => {4..8 => "Split", 10 => "Double or Hit", 12..18 => "Split",20 => "Stand",22 => "Split"},
+  7 => {2..3 => "Split", 4 => "Hit", 5 => "Double or Hit", 6..9 => "Split",20 => "Stand",22 => "Split"},
+  8 => {4 => "Hit", 6 => "Split", 8 => "Hit", 5 => "Double or Hit", 12 => "Hit", 14..18 => "Split",20 => "Stand",22 => "Split"},
+  9 => {4..8 =>"Hit", 10 => "Double or Hit", 12..14 => "Hit",16..18 => "Split",20 => "Stand",22 => "Split"},
+  10 => {4..12 =>"Hit", 14 => "Split",16 => "Pair",18..20 => "Stand",22 => "Split"},
+  "A" => {4..8 =>"Hit",16 => "Pair",18..20 => "Stand",22 => "Split"},
 }
-suggestion = make_suggestions(find_hand_type(card["user_hand"]["first_card"],card["user_hand"]["second_card"]),(card["user_hand"]["first_card"],card["user_hand"]["second_card"]))
+
+def find_hand_type (first, second, dealer, cards)
+  if second == "A" && first == "A"
+    cards["users_hand"]["first_card"] = 11
+    cards["users_hand"]["second_card"] = 11
+    return "pair"
+  elsif first == "A"
+    cards["users_hand"]["first_card"] = 11
+    return "soft"
+  elsif second == "A"
+    cards["users_hand"]["second_card"] = 11
+    return "soft"
+  elsif first == second
+      return "pair"
+  else
+    return "hard"
+  end
+end
+
+def make_suggestion (hand_type, cards)
+  hand = hand_type.select { |dealer| dealer === cards["dealers_card"] }.values.first
+  total_hand = cards["users_hand"]["first_card"] + cards["users_hand"]["second_card"]
+  answer = hand.select { |hand| hand === total_hand }.values.first
+end
+
+
+
+hand_type = find_hand_type(cards["users_hand"]["first_card"],cards["users_hand"]["second_card"],cards["dealers_card"],cards)
+if hand_type == "pair"
+  suggestion = make_suggestion(pair, cards)
+elsif hand_type == "soft"
+  suggestion = make_suggestion(soft, cards)
+else
+  suggestion = make_suggestion(hard, cards)
+end
 puts suggestion
